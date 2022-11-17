111d40dae6696fbada0e5c63432a1e12439d00fa
pavlavl <mr.shchamel@mail.ru>
2022-11-17 10:03:44 +0300

    enums refactor

--- JS/models/ClassificationLevel.js
+++ JS/models/ClassificationLevel.js
@@ -1,6 +1,5 @@
 const ClassificationLevel  = {
     UNCLASSIFIED: 'Unclassified',
-    CONFIDENTIAL: 'Confidential',
     SECRET: 'Secret',
     TOP_SECRET: 'Top_secret'
 };
--- JS/models/ExperimentalTypes.js
+++ JS/models/ExperimentalTypes.js
@@ -1,6 +1,4 @@
 const ExperimentalTypes   = {
-    LIFTING_BODY: 'lifting_body',
-    HYPERSONIC: 'HYPERSONIC',
     HIGH_ALTITUDE: 'HIGH_ALTITUDE',
     VTOL: "VTOL"
 };
